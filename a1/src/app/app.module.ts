e6dcc6a839d53669c97accfb195bfe0a6ddf0c13
Matt Payne <Payne@MattPayne.org>
2021-12-30 02:16:05 +0000

    ng generate component c1

--- a1/src/app/app.module.ts
+++ a1/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { C1Component } from './c1/c1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C1Component
   ],
   imports: [
     BrowserModule,
