ca945c80d2943603e0737740107de7b5b4ee9fa9
Matt Payne <Payne@MattPayne.org>
2021-12-31 02:49:50 +0000

    Done: Setting Up a Complete Navigation System w/ angular material

--- auMaterialClass/src/app/app.module.ts
+++ auMaterialClass/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -12,8 +16,8 @@ import {MatSliderModule} from '@angular/material/slider';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatSliderModule,
+    AppRoutingModule,MatListModule,
+    MatSliderModule,MatSidenavModule,MatToolbarModule,MatIconModule,
     BrowserAnimationsModule
   ],
   providers: [],
